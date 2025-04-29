5059166cf32463fcf0bd155251fce3114201d8e4
YOGESH BAGHEL <83384315+yogesh-hack@users.noreply.github.com>
2025-04-29 16:59:17 +0530

    Update connectMongoDb.js

--- connectMongoDb.js
+++ connectMongoDb.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
 <<<<<<< HEAD
-    const conn = await mongoose.connect("mongodb+srv://stack-overflow-data:2AN3KmaRHo92QRmK@cluster0.jjbvk0s.mongodb.net/", {
+    const conn = await mongoose.connect(process.env.CONNECTION_URL, {
 =======
     const conn = await mongoose.connect(process.env.CONNECTION_URL, {
 >>>>>>> d5424bc97b8a1bc5c3c1dc96843d842885b92a40
