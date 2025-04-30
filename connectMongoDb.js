b59b566df5b0e38e9c8ce95fdea3b2806d32490e
YOGESH BAGHEL <83384315+yogesh-hack@users.noreply.github.com>
2025-04-30 10:18:50 +0530

    Update connectMongoDb.js

--- connectMongoDb.js
+++ connectMongoDb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.CONNECTION_URL, {
+    const conn = await mongoose.connect("mongodb+srv://stack-overflow-data:2AN3KmaRHo92QRmK@cluster0.jjbvk0s.mongodb.net/test", {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
